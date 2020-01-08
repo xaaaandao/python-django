70e50f7e7722d47504008523c9958cf510be71b7
“Alexandre <alexandre.ykz@gmail.com>
2020-01-08 15:39:46 -0300

    Add models e migrate

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# Nosso modelo de dados para virar migration
+
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=9)
+    estoque = models.IntegerField('Quantidade em estoque')
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Nome', max_length=100)
+    email = models.CharField('Nome', max_length=100)
