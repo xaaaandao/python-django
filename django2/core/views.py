dfce3bc8666fdf1d8fe7b99e958e3d61d7238250
Alexandre Kajihara <alexandre.ykz@gmail.com>
2020-01-10 15:32:56 -0300

    Parte 2

---
+++ django2/core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.contrib import messages
+from .forms import ContatoForm
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def contato(request):
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Mensagem enviada')
+            print(f'Nome: {nome}')
+            print(f'E-mail: {email}')
+            print(f'Assunto: {assunto}')
+            print(f'Mensagem: {mensagem}')
+
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = ContatoForm()
+    else:
+        messages.error(request, 'Erro ao enviar e-mail')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
+
+def produto(request):
+    return render(request, 'produto.html')
