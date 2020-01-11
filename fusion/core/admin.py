61fd63544ca549d12146aa279670d603185e6a13
Alexandre Kajihara <alexandre.ykz@gmail.com>
2020-01-11 17:21:10 -0300

    Add models

--- fusion/core/admin.py
+++ fusion/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Cargo, Funcionario, Servico
 
 # Register your models here.
+# É o que aparece lá no admin...
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'modificado')
--- fusion/fusion/settings.py
+++ fusion/fusion/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ngby^zttj5v-8r5^01ad*yx9b=e1o0#ut%jco4@hxzf(a+-9u$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
