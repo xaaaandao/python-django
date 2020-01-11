f4c40d6d08147bb2f34fceb8e15e42e489571276
Alexandre Kajihara <alexandre.ykz@gmail.com>
2020-01-11 12:35:04 -0300

    Ajustando css finais

--- django2/django2/settings.py
+++ django2/django2/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # configurações de e-mail
