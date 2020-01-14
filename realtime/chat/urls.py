687aceefa71c81faee5c76ec4bc0ee6613d12950
Alexandre Kajihara <alexandre.ykz@gmail.com>
2020-01-14 18:15:54 -0300

    add realtime

--- realtime/chat/urls.py
+++ realtime/chat/urls.py
@@ -3,5 +3,5 @@ from .views import IndexView, SalaView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('chat/<str:nome_sala>', SalaView.as_view, name='sala')
+    path('chat/<str:nome_sala>/', SalaView.as_view(), name='sala'),
 ]
--- realtime/realtime/routing.py
+++ realtime/realtime/routing.py
@@ -1,4 +1,4 @@
-from channels.routing import  ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
 
